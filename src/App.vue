ac3de5eaf71c8147260ed501a087fd2b482e8a85
milawu <milawu@urtoday.com>
2024-06-13 15:58:48 +0800

    🐞 fix: delete console

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
     <Content />
-    <Content />
-    <Button />
     <Button />
   </div>
 </template>
@@ -11,7 +9,6 @@
 import { defineAsyncComponent } from 'vue';
 const Content = defineAsyncComponent(() => import("home/Content"));
 const Button = defineAsyncComponent(() => import("home/Button"));
-console.log('test')
 </script>
 
 <style scoped></style>
