197623d45a7a25307d0dc8d2d2cab8c132a5fb5a
milawu <milawu@urtoday.com>
2024-06-13 16:42:07 +0800

    🧪 test: main add console

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 import { defineAsyncComponent } from 'vue';
 const Content = defineAsyncComponent(() => import("home/Content"));
 const Button = defineAsyncComponent(() => import("home/Button"));
+console.log('test3')
 </script>
 
 <style scoped></style>
