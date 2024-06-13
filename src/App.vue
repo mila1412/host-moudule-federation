ae75b4dd1746e0fa116e470ae3949ed6ff4bac56
milawu <milawu@urtoday.com>
2024-06-13 14:38:10 +0800

    🎉 init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Content />
+    <Button />
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+const Content = defineAsyncComponent(() => import("home/Content"));
+const Button = defineAsyncComponent(() => import("home/Button"));
+</script>
+
+<style scoped></style>
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+module.exports = (env = {}) => ({
+  mode: 'development',
+  cache: false,
+  devtool: 'source-map',
+  optimization: {
+    minimize: false,
+    splitChunks: false,
+  },
+  target: 'web',
+  entry: path.resolve(__dirname, './src/index.js'),
+  // output: {
+  //   path: path.resolve(__dirname, './dist'),
+  //   publicPath: '/dist/'
+  // },
+  output: {
+    publicPath: 'auto',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.vue', '.jsx', '.js', '.json'],
+    alias: {
+      vue: '@vue/compat/dist/vue.esm-bundler.js',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 8192 },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new ModuleFederationPlugin({
+      name: 'home',
+      filename: 'remoteEntry.js',
+      remotes: {
+        home: 'home@http://localhost:3002/remoteEntry.js',
+      },
+      shared: {
+        vue: {
+          eager: true,
+          singleton: true,
+          requiredVersion: '^3.1.0',
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html'),
+    }),
+    new VueLoaderPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+    port: 3001,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+  },
+});
