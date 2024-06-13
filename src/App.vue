257b08f3604f20ef3a5409c23ec3af5e09ed2db3
milawu <milawu@urtoday.com>
2024-06-13 16:01:18 +0800

    🧪 test: fix conflict 2

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
 import { defineAsyncComponent } from 'vue';
 const Content = defineAsyncComponent(() => import("home/Content"));
 const Button = defineAsyncComponent(() => import("home/Button"));
-console.log('test3')
 </script>
 
 <style scoped></style>
